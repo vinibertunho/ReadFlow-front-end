f24b6fdde4ac9998e6dd857fad28aa0ad17e79fb
julia <juhcdgomes@gmail.com>
2026-05-08 10:52:27 -0300

    refactor: remove unused CSS and components; restructure App component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+<nav className="navbar">
+    <div className="nav-brand">ReadFlow</div>
+    <ul className="nav-links">
+        <li className="active">Início</li>
+        <li>Biblioteca</li>
+        <li>Explorar obra</li>
+        <li>Equipe</li>
+        <li>Vestibulandos</li>
+        <li>Quiz</li>
+        <li>Vídeos</li>
+        <li>Curiosidades</li>
+    </ul>
+    <div className="nav-lang">PT/EN 🌐</div>
+</nav>;
