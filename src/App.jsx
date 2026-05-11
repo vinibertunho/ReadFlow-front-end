3e59aace99c91703c89b19ad94dd0dc0846bc790
vini <vinicius.m.silva36@aluno.senai.br>
2026-05-11 11:49:28 -0300

    faz a biblioteca de livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+
 import Biblioteca from './pages/Biblioteca de livros/Biblioteca';
-import Curiosidades from './pages/Curiosidades e dicas/Curiosidades';
-import Equipe from './pages/Equipe/Equipe';
-import Videoaulas from './pages/Videoaulas/Videoaulas';
-import Vestibular from './pages/Vestibular/Vestibular';
-import Simulados from './pages/Simulados e quiz/Simulados';
-import Livro from './pages/Livro/Livro';
+
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/biblioteca" element={<Biblioteca />} />
-                <Route path="/curiosidades" element={<Curiosidades />} />
-                <Route path="/equipe" element={<Equipe />} />
-                <Route path="/videoaulas" element={<Videoaulas />} />
-                <Route path="/vestibular" element={<Vestibular />} />
-                <Route path="/simulados" element={<Simulados />} />
-                <Route path="/livro/:id" element={<Livro />} />
-            </Routes>
-        </BrowserRouter>
+    <>
+        <Biblioteca />
+    </>
     );
 }
+
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
+import { Navigate } from 'react-router-dom';
 import styles from './Home.module.css';
 
+void styles;
 
 export default function Home() {
-    
+	return <Navigate to="/biblioteca" replace />;
 }
