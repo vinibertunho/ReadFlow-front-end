2ceadc6cebf573075c24e5366abfd36a76a0c1df
vini <vinicius.m.silva36@aluno.senai.br>
2026-05-15 14:06:46 -0300

    arruma o livro

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,38 +10,38 @@ function Home() {
                     <div className={styles.criancasCorrendo}>
                         
                     </div>
-                </div>
-            </section>
-            <section className={styles.destaques}>
-                <div className={styles.apresentacao}>
-                    <h3>Apresentação do Projeto</h3>
-                    <p>Apresentacao do projeto aqui</p>
-                </div>
-                <div className={styles.cards}>
-                    <div className={styles.explorarObra}>
-                        <h4>Explorar obra</h4>
-                        <p>Acompanhe a narrativa...</p>
-                        <p>Ler Análise → </p>
+                </section>
+                <section className={styles.destaques}>
+                    <div className={styles.apresentacao}>
+                        <h3>Apresentação do Projeto</h3>
+                        <p>Apresentacao do projeto aqui</p>
                     </div>
-                    <div className={styles.equipe}>
-                        <h4>Equipe</h4>
-                        <p>Conheça a equipe por trás do projeto...</p>
-                        <p>Conhecer → </p>
-                    </div>
-                    <div className={styles.vestibulandos}>
-                        <h4>Vestibulandos</h4>
-                        <p>Página dedicada aos vestibulandos...</p>
-                        <p>Estudar → </p>
-                    </div>
-                    <div className={styles.simulados}>
-                        <h4>Simulados e Quizes</h4>
-                        <p>Uooooou</p>
-                        <p>Ver tester → </p>
-                    </div>
-                    <div className={styles.videoaulas}>
-                        <h4>Videoaulas</h4>
-                        <p>Assista às videoaulas...</p>
-                        <p>Ver Galeria → </p>
+                    <div className={styles.cards}>
+                        <div className={styles.explorarObra}>
+                            <h4>Explorar obra</h4>
+                            <p>Acompanhe a narrativa...</p>
+                            <p>Ler Análise → </p>
+                        </div>
+                        <div className={styles.equipe}>
+                            <h4>Equipe</h4>
+                            <p>Conheça a equipe por trás do projeto...</p>
+                            <p>Conhecer → </p>
+                        </div>
+                        <div className={styles.vestibulandos}>
+                            <h4>Vestibulandos</h4>
+                            <p>Página dedicada aos vestibulandos...</p>
+                            <p>Estudar → </p>
+                        </div>
+                        <div className={styles.simulados}>
+                            <h4>Simulados e Quizes</h4>
+                            <p>Uooooou</p>
+                            <p>Ver tester → </p>
+                        </div>
+                        <div className={styles.videoaulas}>
+                            <h4>Videoaulas</h4>
+                            <p>Assista às videoaulas...</p>
+                            <p>Ver Galeria → </p>
+                        </div>
                     </div>
                 </section>
             </main>
